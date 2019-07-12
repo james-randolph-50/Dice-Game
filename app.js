35b2b32dd82d595aca5891f506e5e84e43bc6d09
James Randolph <james.randolph2016@gmail.com>
2019-07-12 09:37:32 -0400

    innerHTML + styling

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
