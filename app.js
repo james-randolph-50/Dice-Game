30e9549e8f06b3c162e9cea52d705e1f47787c6c
James Randolph <james.randolph2016@gmail.com>
2019-07-23 10:17:29 -0400

    change textContext to textContent

--- app.js
+++ app.js
@@ -4,15 +4,33 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-// var x = document.querySelector('#score-0').textContent;
-
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    let diceDOM = document.querySelector('.dice');
+    // Get random number (rolls dice)
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // Displays the dice
+    var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    // Update the round score IF the rolled number !== 1
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+
+    }
 });
+
+
+
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
